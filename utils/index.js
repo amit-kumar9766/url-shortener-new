202b932ce88cafaa07d1672f43daebef4dcf700a
Amit Kumar Balhara <amitbalharakr93@gmail.com>
2026-04-01 21:56:16 +0530

    removed unnecessary files

--- utils/index.js
+++ utils/index.js
@@ -4,15 +4,15 @@ const { Url } = require("../models");
 exports.generateShortUrl = async ({ url, userId, expiryDate }) => {
   // Check if URL already exists for user
   const existing = await Url.findOne({ where: { originalUrl: url, userId } });
-  if (existing) return { existing: true, shortCode: existing.shortCode };
+  if (existing) return { existing: true, shortUrl: existing.shortUrl };
 
   const code = crypto.randomBytes(3).toString("hex");
   const newUrl = await Url.create({
     originalUrl: url,
-    shortCode: code,
+    shortUrl: code,
     userId,
     expiryDate: expiryDate ? new Date(expiryDate) : null,
   });
 
-  return { existing: false, shortCode: newUrl.shortCode };
+  return { existing: false, shortUrl: newUrl.shortUrl };
 };
